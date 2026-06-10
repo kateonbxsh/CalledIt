72ba22ddc1d041ecf20af00b204536013ef317a7
Aouab <aouabsss@gmail.com>
2026-06-10 05:44:55 +0200

    add: padding to rank legend component
    RankLegend wrapper:
    - Added px-4 py-2 padding for breathing room
    - Separates scale from surrounding content
    - Cleaner layout on leaderboard page

--- src/components/RankLegend.tsx
+++ src/components/RankLegend.tsx
@@ -20,7 +20,7 @@ export function RankLegend() {
   const getX = (elo: number) => ((elo - 300) / (maxElo - 300)) * SVG_WIDTH;
 
   return (
-    <div className="space-y-4">
+    <div className="px-4 py-2">
       <svg
         width="100%"
         height={SVG_HEIGHT}
